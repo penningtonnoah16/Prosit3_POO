bc570c08084f024e079fad2e914de00125f9f892
Moi <amayas.yalaoui@viacesi.fr>
2025-11-19 11:55:45 +0100

    Modifications de polyferme

---
+++ polyferme.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
+namespace formes{
+    class Polyferme {
+        public : 
+            Polyferme(double, double, double, double);
+    }
+}
